0282b0e555ae284afbf83d9a5ec1b2fc05595419
ThamsanqaMpontshane <thamsanqalm01@gmail.com>
2024-09-17 20:36:21 +0200

    Add detailed print statements for stock prices and ratio

--- client3.py
+++ client3.py
@@ -31,18 +31,19 @@ N = 500
 
 def getDataPoint(quote):
     """ Produce all the needed values to generate a datapoint """
-    """ ------------- Update this function ------------- """
     stock = quote['stock']
     bid_price = float(quote['top_bid']['price'])
     ask_price = float(quote['top_ask']['price'])
-    price = bid_price
+    price = (bid_price + ask_price) / 2  # Calculate the average of bid and ask prices
     return stock, bid_price, ask_price, price
 
 
 def getRatio(price_a, price_b):
     """ Get ratio of price_a and price_b """
     """ ------------- Update this function ------------- """
-    return 1
+    if price_b == 0:
+        return
+    return price_a / price_b
 
 
 # Main
@@ -52,8 +53,11 @@ if __name__ == "__main__":
         quotes = json.loads(urllib.request.urlopen(QUERY.format(random.random())).read())
 
         """ ----------- Update to get the ratio --------------- """
+        prices = {}
         for quote in quotes:
             stock, bid_price, ask_price, price = getDataPoint(quote)
-            print("Quoted %s at (bid:%s, ask:%s, price:%s)" % (stock, bid_price, ask_price, price))
+            prices[stock] = price
+            print(f"Stock: {stock}, Bid Price: {bid_price}, Ask Price: {ask_price}, Price: {price}")
 
-        print("Ratio %s" % getRatio(price, price))
+        ratio = getRatio(prices.get('ABC', 0), prices.get('DEF', 0))
+        print(f"Ratio: {ratio}")
